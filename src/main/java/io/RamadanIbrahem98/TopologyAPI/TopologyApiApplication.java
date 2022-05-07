7a9fd364e0e8e26b9112ac456f58f222391b8403
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2022-05-07 18:37:05 +0200

    feat: Spring Initializr Files

---
+++ src/main/java/io/RamadanIbrahem98/TopologyAPI/TopologyApiApplication.java
@@ -0,0 +1,14 @@
+package io.RamadanIbrahem98.TopologyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TopologyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopologyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/RamadanIbrahem98/TopologyAPI/TopologyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.RamadanIbrahem98.TopologyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopologyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
